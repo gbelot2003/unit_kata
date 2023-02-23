53dbebbc4525961d60e71d76b199c2d2d25cef21
Administrator <admin@example.com>
2023-02-22 22:59:56 -0600

    Modularizando el problema

--- src/checkout.ts
+++ src/checkout.ts
@@ -3,15 +3,48 @@ const B_PRICE = 30;
 const C_PRICE = 20;
 const D_PRICE = 15;
 
+interface Item {
+    getPrice(): number
+}
+
+class A implements Item {
+    getPrice(): number {
+        return A_PRICE
+    }
+}
+
+class B implements Item {
+    getPrice(): number {
+        return B_PRICE
+    }
+}
+
+class C implements Item {
+    getPrice(): number {
+        return C_PRICE
+    }
+}
+
+class D implements Item {
+    getPrice(): number {
+        return D_PRICE
+    }
+}
+
+function itemFactory(itemChar: string) : Item {
+    if (itemChar === 'A') return new A()
+    if (itemChar === 'B') return new B()
+    if (itemChar === 'C') return new C()
+    if (itemChar === 'D') return new D()
+    throw new Error("Something bad happened")
+}
+
 export function checkout(itemsList: string): number {
 
     let total = 0;
-    for (const item of itemsList.split("")) {
-        if (item === 'A') total += A_PRICE
-        else if (item === 'B') total += B_PRICE
-        else if (item === 'C') total += C_PRICE
-        else if (item === 'D') total += D_PRICE
-        else throw Error("Something bad happened")
+    for (const itemChar of itemsList.split("")) {
+        const item = itemFactory(itemChar)
+        total += item.getPrice()
     }
 
     return total;
